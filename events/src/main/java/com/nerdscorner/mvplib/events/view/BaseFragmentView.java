68cae1def100d102692e63285b4d16a18840233d
Diego Marcher <diego@marcher.com.uy>
2018-11-02 13:14:43 -0300

    fragment manager

--- events/src/main/java/com/nerdscorner/mvplib/events/view/BaseFragmentView.java
+++ events/src/main/java/com/nerdscorner/mvplib/events/view/BaseFragmentView.java
@@ -1,10 +1,11 @@
 package com.nerdscorner.mvplib.events.view;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 import com.nerdscorner.mvplib.commons.mvp.view.BaseView;
 import com.nerdscorner.mvplib.events.bus.Bus;
@@ -28,9 +29,15 @@ public abstract class BaseFragmentView extends BaseView {
 
     @Nullable
     @Override
-    public Activity getActivity() {
+    public FragmentActivity getActivity() {
         Fragment fragment = fragmentRef.get();
         return (fragment == null) ? null : fragment.getActivity();
+    }
+
+    @Nullable
+    public FragmentManager getFragmentManager() {
+        FragmentActivity activity = getActivity();
+        return activity != null ? activity.getSupportFragmentManager() : null;
     }
 
     @Nullable
--- interfaces/src/main/java/com/nerdscorner/mvplib/interfaces/view/BaseFragmentView.java
+++ interfaces/src/main/java/com/nerdscorner/mvplib/interfaces/view/BaseFragmentView.java
@@ -1,10 +1,11 @@
 package com.nerdscorner.mvplib.interfaces.view;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 import com.nerdscorner.mvplib.interfaces.presenter.BaseFragmentPresenter;
 
@@ -19,9 +20,15 @@ public abstract class BaseFragmentView<P extends BaseFragmentPresenter> extends 
     }
     @Nullable
     @Override
-    public Activity getActivity() {
+    public FragmentActivity getActivity() {
         Fragment fragment = fragmentRef.get();
         return (fragment == null) ? null : fragment.getActivity();
+    }
+
+    @Nullable
+    public FragmentManager getFragmentManager() {
+        FragmentActivity activity = getActivity();
+        return activity != null ? activity.getSupportFragmentManager() : null;
     }
 
     @Nullable
