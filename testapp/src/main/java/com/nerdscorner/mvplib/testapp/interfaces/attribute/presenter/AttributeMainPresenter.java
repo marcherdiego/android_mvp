d518825d20e8a99e4596922a1b2596bf576c908b
Diego Marcher <diego@marcher.com.uy>
2018-11-03 17:59:17 -0300

    tweaks

--- testapp/src/main/java/com/nerdscorner/mvplib/testapp/interfaces/attribute/presenter/AttributeMainPresenter.java
+++ testapp/src/main/java/com/nerdscorner/mvplib/testapp/interfaces/attribute/presenter/AttributeMainPresenter.java
@@ -4,18 +4,16 @@ import com.nerdscorner.mvplib.interfaces.presenter.BaseActivityPresenter;
 import com.nerdscorner.mvplib.testapp.interfaces.attribute.model.AttributeMainModel;
 import com.nerdscorner.mvplib.testapp.interfaces.attribute.view.AttributeMainView;
 
-public class AttributeMainPresenter extends BaseActivityPresenter<AttributeMainView, AttributeMainModel> implements AttributeMainPresenterInterface {
+public class AttributeMainPresenter extends BaseActivityPresenter<AttributeMainView, AttributeMainModel>{
 
     public AttributeMainPresenter(AttributeMainView view, AttributeMainModel model) {
         super(view, model);
     }
 
-    @Override
     public void onActionClicked() {
         model.doSomethingInBackground();
     }
 
-    @Override
     public void onBackgroundTaskCompleted() {
         view.setTextValue("Background task completed");
     }
