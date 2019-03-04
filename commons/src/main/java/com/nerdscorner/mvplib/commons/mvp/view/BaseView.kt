18919f9783aadfeb7f3d7a7f1f49e5ecb273b3f1
Diego Marcher <diego@marcher.com.uy>
2019-03-04 20:53:43 -0300

    added abstract activity property

--- commons/src/main/java/com/nerdscorner/mvplib/commons/mvp/view/BaseView.kt
+++ commons/src/main/java/com/nerdscorner/mvplib/commons/mvp/view/BaseView.kt
@@ -5,6 +5,7 @@ import android.support.annotation.StringRes
 import android.widget.Toast
 
 abstract class BaseView {
+    abstract val activity: Activity?
 
     fun unbind() {}
 
@@ -19,34 +20,32 @@ abstract class BaseView {
     fun onStart() {}
 
     fun showToast(@StringRes textResId: Int) {
-        val activity = getActivity() ?: return
+        val activity = activity ?: return
         Toast.makeText(activity, textResId, Toast.LENGTH_SHORT).show()
     }
 
     fun showToast(@StringRes textResId: Int, vararg args: Any) {
-        val activity = getActivity() ?: return
+        val activity = activity ?: return
         Toast.makeText(activity, activity.getString(textResId, *args), Toast.LENGTH_SHORT).show()
     }
 
     fun showToast(text: String) {
-        val activity = getActivity() ?: return
+        val activity = activity ?: return
         Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
     }
 
     fun showToast(duration: Int, @StringRes textResId: Int) {
-        val activity = getActivity() ?: return
+        val activity = activity ?: return
         Toast.makeText(activity, activity.getString(textResId), duration).show()
     }
 
     fun showToast(duration: Int, text: String) {
-        val activity = getActivity() ?: return
+        val activity = activity ?: return
         Toast.makeText(activity, text, duration).show()
     }
 
     fun showToast(duration: Int, @StringRes textResId: Int, vararg args: Any) {
-        val activity = getActivity() ?: return
+        val activity = activity ?: return
         Toast.makeText(activity, activity.getString(textResId, *args), duration).show()
     }
-
-    abstract fun getActivity(): Activity?
 }
